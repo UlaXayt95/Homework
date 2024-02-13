6344da80ecb9d439556809f7912c51cde0c794cf
Ulugbek <UlaXayt95gmail.com>
2024-02-13 12:06:12 +0300

    second commit

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Улугбек Хайтметов on 29.01.2024.
+//
+
+import Foundation
---
+++ Navigation/Profile/ProfileeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Navigation
+//
+//  Created by Улугбек Хайтметов on 29.01.2024.
+//
+import Foundation
+import UIKit
+
+class ProfileeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+
+    }
+}
+
+
